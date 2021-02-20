483dcf7be00f34a06113105d3f770ab743cbe75c
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2021-02-20 13:57:54 +0300

    Complete detail page development
    - Firebase Analytics Log
    - Movie details

---
+++ Moovie/Utilities/Constants/AnalyticsConstants.swift
@@ -0,0 +1,22 @@
+//
+//  AnalyticsConstants.swift
+//  Moovie
+//
+//  Created by Bugra's Mac on 20.02.2021.
+//
+
+import Foundation
+
+extension Constant {
+    
+    class AnalyticsConstants {
+        static let logMovieDetailEvent = "movie_detail_viewed"
+        static let title = "title"
+        static let imdbRating = "imdbRating"
+        static let reviewCount = "reviewCount"
+        static let relased = "relased"
+        static let director = "director"
+        static let null = "null"
+    }
+    
+}
