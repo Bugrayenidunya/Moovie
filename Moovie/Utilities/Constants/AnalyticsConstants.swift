2945ebc42c6774e2dfb41af5e77656303661ffb3
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2021-02-20 19:29:10 +0300

    Refactor AnalyticsConstants class

--- Moovie/Utilities/Constants/AnalyticsConstants.swift
+++ Moovie/Utilities/Constants/AnalyticsConstants.swift
@@ -11,12 +11,6 @@ extension Constant {
     
     class AnalyticsConstants {
         static let logMovieDetailEvent = "movie_detail_viewed"
-        static let title = "title"
-        static let imdbRating = "imdbRating"
-        static let reviewCount = "reviewCount"
-        static let relased = "relased"
-        static let director = "director"
-        static let null = "null"
     }
     
 }
